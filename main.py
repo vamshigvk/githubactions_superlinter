d7b518151b7be50836eda84e5051674afbb038f6
G Vamshi Krishna <vamshikrishnagvk5@gmail.com>
2021-04-03 17:17:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@
   
 # function find sttring greater than length k
 def string_k(k, str):
-      
+
     # create the empty string
     string = []
-      
+
     # split the string where space is comes
     text = str.split(" ")
-      
+
     # iterate the loop till every substring
     for x in text:
-          
+
         # if length of current sub string
         # is greater than k then
         if len(x) > k:
@@ -20,11 +20,11 @@ def string_k(k, str):
             # append this sub string in
             # string list
             string.append(x)
-              
+
      # return string list
     return string
-  
-  
+
+
 # Driver Program     
 k = 3
 str ="geek for geeks"
